080ffe331f2c8236bafc59ea85280fd0299e200a
Yuliia Koba <ykoba@clipmoney.com>
2024-12-09 00:23:03 +0200

    init main pages

---
+++ presentation/lib/src/partners/partners_page/partners_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PartnersPage extends StatelessWidget {
+  const PartnersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('PartnersPage'),
+      ),
+    );
+  }
+}
