e5d8cabbb39e0c48243c6842c8c31256e8a3329b
Yuliia Koba <ykoba@clipmoney.com>
2025-02-10 22:06:21 +0200

    fix ControllersMiddleware

--- executable/lib/middlewares/controllers_middleware.dart
+++ executable/lib/middlewares/controllers_middleware.dart
@@ -1,6 +1,7 @@
 import 'package:clean_redux/clean_redux.dart';
 import 'package:data/data.dart';
 import 'package:domain/domain.dart';
+import 'package:executable/controllers/criterias_controller.dart';
 import 'package:executable/controllers/partners_controller.dart';
 import 'package:executable/controllers/tables_controller.dart';
 import 'package:executable/controllers/user_controller.dart';
@@ -20,6 +21,7 @@ class ControllersMiddleware extends EpicMiddleware<AppState> {
                 ...Controller([Endpoint(ReloadData()).call]),
                 ...UserController(store, userRepository),
                 ...PartnersController(store, partnersRepository),
+                ...CriteriasController(store, criteriasRepository),
                 ...TablesController(
                   store,
                   partnersRepository,
--- presentation/lib/src/criterias/criterias_page/criterias_page.dart
+++ presentation/lib/src/criterias/criterias_page/criterias_page.dart
@@ -1,13 +1,21 @@
+import 'package:domain/domain.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
 class CriteriasPage extends StatelessWidget {
   const CriteriasPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      body: const Center(
         child: Text('CriteriasPage'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => StoreProvider.of<AppState>(context).dispatch(
+          OpenPageAction(Destination.createCriteria),
+        ),
       ),
     );
   }
