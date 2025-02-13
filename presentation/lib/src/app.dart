9b70ba5c3c0fc590846a57f9ff5485a3cdfd9578
Yuliia Koba <ykoba@clipmoney.com>
2025-02-13 18:30:34 +0200

    add updates

--- presentation/lib/src/app.dart
+++ presentation/lib/src/app.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:localizations/localizations.dart';
 import 'package:presentation/src/core/app_theme.dart';
+import 'package:presentation/src/core/base_view_model.dart';
 import 'package:presentation/src/navigation/app_route_info_provider.dart';
 import 'package:presentation/src/navigation/app_router_delegate.dart';
 import 'package:presentation/src/navigation/parsing/app_route_parser.dart';
@@ -35,47 +36,61 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     final store = StoreProvider.of<AppState>(context);
-    final languageCode = store.state.settingsState.languageCode;
 
-    final locale = SynergyScannerLocalizations.supportedLocales
-            .firstWhereOrNull(
-                (locale) => locale.languageCode == languageCode) ??
-        SynergyScannerLocalizations.supportedLocales.first;
-
-    return AnnotatedRegion<SystemUiOverlayStyle>(
-      value: const SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent,
-        statusBarBrightness: Brightness.light, // For iOS
-        statusBarIconBrightness: Brightness.light, // For Android
-      ),
-      child: MaterialApp.router(
-        title: 'Synergy Scanner',
-        localizationsDelegates:
-            SynergyScannerLocalizations.localizationsDelegates,
-        supportedLocales: SynergyScannerLocalizations.supportedLocales,
-        locale: locale,
-        theme: lightTheme,
-        darkTheme: darkTheme,
-        themeMode: switch (store.state.settingsState.theme) {
-          AppTheme.light => ThemeMode.light,
-          AppTheme.dark => ThemeMode.dark,
-          AppTheme.system => ThemeMode.system,
-        },
-        routerDelegate: AppRouterDelegate(store, routes),
-        routeInformationParser: AppRouteParser(
-          () => store.state.currentUserState.user,
+    return StoreConnector<AppState, _ViewModel>(
+      distinct: true,
+      converter: _ViewModel.new,
+      builder: (context, viewModel) => AnnotatedRegion<SystemUiOverlayStyle>(
+        value: const SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarBrightness: Brightness.light, // For iOS
+          statusBarIconBrightness: Brightness.light, // For Android
         ),
-        routeInformationProvider: AppRouteInformationProvider(
-          initialRouteInformation: RouteInformation(
-            uri: Uri.parse(
-              WidgetsBinding.instance.platformDispatcher.defaultRouteName,
-            ),
+        child: MaterialApp.router(
+          title: 'Synergy Scanner',
+          localizationsDelegates:
+              SynergyScannerLocalizations.localizationsDelegates,
+          supportedLocales: SynergyScannerLocalizations.supportedLocales,
+          locale: viewModel.locale,
+          theme: lightTheme,
+          darkTheme: darkTheme,
+          themeMode: viewModel.themeMode,
+          routerDelegate: AppRouterDelegate(store, routes),
+          routeInformationParser: AppRouteParser(
+            () => store.state.currentUserState.user,
           ),
-          store: store,
+          routeInformationProvider: AppRouteInformationProvider(
+            initialRouteInformation: RouteInformation(
+              uri: Uri.parse(
+                WidgetsBinding.instance.platformDispatcher.defaultRouteName,
+              ),
+            ),
+            store: store,
+          ),
+          backButtonDispatcher: _backButtonDispatcher,
+          debugShowCheckedModeBanner: false,
         ),
-        backButtonDispatcher: _backButtonDispatcher,
-        debugShowCheckedModeBanner: false,
       ),
     );
   }
+}
+
+class _ViewModel extends BaseViewModel {
+  final Locale locale;
+  final ThemeMode themeMode;
+
+  _ViewModel(super.store)
+      : locale = SynergyScannerLocalizations.supportedLocales.firstWhereOrNull(
+                (locale) =>
+                    locale.languageCode ==
+                    store.state.settingsState.languageCode) ??
+            SynergyScannerLocalizations.supportedLocales.first,
+        themeMode = switch (store.state.settingsState.theme) {
+          AppTheme.light => ThemeMode.light,
+          AppTheme.dark => ThemeMode.dark,
+          AppTheme.system => ThemeMode.system,
+        };
+
+  @override
+  List<Object?> get props => [locale, themeMode];
 }
